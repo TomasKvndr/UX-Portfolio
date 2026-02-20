5881f4da7d479efb469d87b632865d7cfea17340
TomasKvndr <144017336+TomasKvndr@users.noreply.github.com>
2026-02-20 12:14:02 +0100

    pomee

--- src/app/components/SkaterPortfolio.tsx
+++ src/app/components/SkaterPortfolio.tsx
@@ -9,10 +9,10 @@ import { ArrowUpRight, Terminal, Activity, Globe, FileText } from 'lucide-react'
 */
 
 
-import primeclinic from "@/assets/primeclinic.png";
-import racesim from "@/assets/racesim.png";
-import farmavet from "@/assets/farmavet.png";
-import kreslo from "@/assets/kreslo.png";
+import primeclinic from "../../assets/primeclinic.png";
+import racesim from "../../assets/racesim.png";
+import farmavet from "../../assets/farmavet.png";
+import kreslo from "../../assets/kreslo.png";
 
 
 const ASSETS = {
