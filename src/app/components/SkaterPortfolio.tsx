5fcde647a6e97901f03075bd25a13e3a6c8f81c7
TomasKvndr <144017336+TomasKvndr@users.noreply.github.com>
2026-02-26 12:17:31 +0100

    case study ku tomu

--- src/app/components/SkaterPortfolio.tsx
+++ src/app/components/SkaterPortfolio.tsx
@@ -228,13 +228,29 @@ export const SkaterPortfolio = () => {
                            <h4 className="text-3xl md:text-5xl font-black uppercase tracking-tight flex items-center gap-4">
                               {project.title}
                               {project.special && (
+                                 
                                  <a 
    href="https://test-debug-63257048.figma.site/#" 
    target="_blank" 
    rel="noopener noreferrer"
    className="text-[10px] bg-white text-black px-2 py-1 rounded-full font-bold uppercase tracking-widest transition-colors hover:bg-black hover:text-[#D9FF00] cursor-pointer inline-block"
 >
-   EXPLORE LIVE DEMO
+   CLICK HERE FOR LIVE DEMO
+</a>
+                                 
+                              )}
+                           </h4>
+                           <h4 className="text-3xl md:text-5xl font-black uppercase tracking-tight flex items-center gap-4">
+                              {project.title}
+                              {project.special && (
+                                 
+                                 <a 
+   href="https://test-debug-63257048.figma.site/#" 
+   target="_blank" 
+   rel="noopener noreferrer"
+   className="text-[10px] bg-white text-black px-2 py-1 rounded-full font-bold uppercase tracking-widest transition-colors hover:bg-black hover:text-[#D9FF00] cursor-pointer inline-block"
+>
+   CASE STUDY AVAILABLE
 </a>
                                  
                               )}
